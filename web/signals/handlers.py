874a5b823b65cd6739e99fb839d67ad5e4e810fd
erfanAliaghdam <80113382+erfanAliaghdam@users.noreply.github.com>
2022-06-13 16:57:18 +0430

    created signals for RequestProjCart

---
+++ web/signals/handlers.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_save
+from django.conf import settings
+from django.dispatch import receiver
+from web.models import RequestedProjects, ProfileMentor, ProfileStud
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile_for_new_user(sender, **kwargs):
+    if kwargs['created']:
+        if kwargs['instance'].__dict__['is_mentor']:
+            ProfileMentor.objects.create(user=kwargs['instance'])
+            print("-----automatically: Mentor Profile-----")
+        else:
+            ProfileStud.objects.create(user=kwargs['instance'])
+            print("-----automatically: Stud Profile-----")
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_RequestedProjRelation_for_new_user(sender, **kwargs):
+    if kwargs['created']:
+        RequestedProjects.objects.create(user=kwargs['instance'])
+        print("-----automatically: RequestedProjectCart-----")
