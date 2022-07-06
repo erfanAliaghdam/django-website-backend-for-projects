58cbdb54f1f1a6e8b632889db7a96facb647f4b9
erfanAliaghdam <80113382+erfanAliaghdam@users.noreply.github.com>
2022-07-06 20:26:24 +0430

    test for project is ready

---
+++ core/api/serializers 2.py
@@ -0,0 +1,50 @@
+from dataclasses import fields
+from pyexpat import model
+from djoser.serializers import (UserSerializer as BaseUserSerializer,
+                                 UserCreateSerializer as BaseUserCreateSerializer)
+from rest_framework import serializers
+from ..models import Message, User
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'first_name', 'last_name', 'phone', 'is_mentor', 'resume']
+        
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id', 'first_name', 'last_name', 'phone', 'is_mentor', 'password']
+    
+
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = Message
+        fields = ['id', 'reciever', 'message', 'send_time', 'message_type']
+        related_fields = ['reciever']    
+
+
+
+
+class CreateTokenSerializer(serializers.Serializer):
+    phone = serializers.CharField()
+    class Meta:
+        model  = User
+        fields = ['phone']
+
+
+class SendOtpSerializer(serializers.Serializer):
+    phone = serializers.CharField()
+    otp = serializers.CharField(max_length=6)
+    class Meta:
+        model  = User
+        fields = ['otpCode', 'phone']
+
+
+class SignUpOtpSerializer(serializers.Serializer):
+    phone = serializers.CharField()
+    class Meta:
+        model  = User
+        fields = ['otpCode', 'phone', 'password']
+
--- web/tests/projects_test.py
+++ web/tests/projects_test.py
@@ -2,18 +2,17 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.contrib.auth import get_user_model
 import pytest
-user_obj = get_user_model()
 
-@pytest.mark.django_db
+
 class TestCreateProject:
     def test_if_user_is_anonymous_return_401(self):
         client = APIClient()
-        response = client.post('/api/projects/', {'title': 'test', 'description': 'test', 'tag': 4})
+        response = client.post('/api/projects/', {'title': 'tfest', 'description': 'test', 'tag': 4, 'admissionNo':1})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     def test_if_user_is_not_an_mentor_return_401(self):
+        user_obj = get_user_model()
         client   = APIClient()
         client.force_authenticate(user = user_obj(is_mentor = False))
-        response = client.post('/api/projects/', {'title': 'test', 'description': 'test', 'tag': 4})
-        print(response)
+        response = client.post('/api/projects/', {'title': 'test', 'description': 'test', 'tag': 4,  'admissionNo':1})
         assert response.status_code == status.HTTP_403_FORBIDDEN
